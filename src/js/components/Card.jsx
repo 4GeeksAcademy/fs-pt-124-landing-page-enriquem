ac38b94b4d4b6b933c2e175d30c8a7d11e3d209d
Enrique A <emoralesdelosrios@icloud.com>
2025-11-28 11:21:27 +0000

    se arreglo el componente card, se ven ahora todos del mismo tamaño y el boton esta abajo y centrado

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const Card = (props) => {
     return (
-        <div className="col-lg-3 col-md-6 col-sm-12">
+        <div className="col-lg-3 col-md-6 col-sm-12 d-flex">
         <div className="card p-0 my-3 text-center">
             <div className="ratio ratio-1x1">
             <img src={props.img} className="object-fit-cover" alt="..."/>
             </div>
-                <div className="card-body">
+                <div className="card-body d-flex flex-column">
                     <h5 className="card-title">{props.title}</h5>
                     <p className="card-text">{props.description}</p>
-                    <a href={props.wiki} className="btn btn-primary">Wikipedia</a>
+                    <a href={props.wiki} className="wikiBottom w-50 mt-auto btn btn-primary">Wikipedia</a>
                 </div>
-        </div>
+            </div>
         </div>
     )
 }
